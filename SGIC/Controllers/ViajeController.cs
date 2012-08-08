91f9b8248e6b1213f73c68139e7c1ecbc7a42969
Eze <ezequiel.reyno@gmail.com>
2012-08-08 01:32:08 -0300

    Include information for entity framework

---
+++ SGIC/Controllers/ViajeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SGIC.Models;
+
+namespace SGIC.Controllers
+{ 
+    public class ViajeController : Controller
+    {
+        private SGICContext db = new SGICContext();
+
+        //
+        // GET: /Viaje/
+
+        public ViewResult Index()
+        {
+            return View(db.Viajes.ToList());
+        }
+
+        //
+        // GET: /Viaje/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Viaje viaje = db.Viajes.Find(id);
+            return View(viaje);
+        }
+
+        //
+        // GET: /Viaje/Create
+
+        public ActionResult Create()
+        {
+            Viaje model = new Viaje();
+            return View(model);
+        } 
+
+        //
+        // POST: /Viaje/Create
+
+        [HttpPost]
+        public ActionResult Create(Viaje viaje)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Viajes.Add(viaje);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(viaje);
+        }
+        
+        //
+        // GET: /Viaje/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Viaje viaje = db.Viajes.Find(id);
+            return View(viaje);
+        }
+
+        //
+        // POST: /Viaje/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Viaje viaje)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(viaje).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(viaje);
+        }
+
+        //
+        // GET: /Viaje/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Viaje viaje = db.Viajes.Find(id);
+            return View(viaje);
+        }
+
+        //
+        // POST: /Viaje/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Viaje viaje = db.Viajes.Find(id);
+            db.Viajes.Remove(viaje);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- SGIC/Models/Viaje.cs
+++ SGIC/Models/Viaje.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SGIC.Models
 {
     public class Viaje
     {
+        public Viaje()
+        {
+            fecha = DateTime.Today;
+        }
+        [Key]
+        public int id { get; set; }
         public DateTime fecha { get; set; }
+        [Display(Name = "Numero de Viaje")]
         public int numeroViaje { get; set; }
+        [Display(Name = "Monto Efectivo")]
         public decimal efectivo { get; set; }
+        [Display(Name = "Monto Cta. Cte.")]
         public decimal importe { get; set; }
+        [Display(Name = "Monto Peaje")]
         public decimal peaje { get; set; }
     }
 }
